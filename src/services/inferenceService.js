e0dac8848b8cb75d6aa04186f1c40f8c02ffb257
Yousran <yusranmazidan@gmail.com>
2024-12-07 16:37:31 +0800

    Refine inference suggestions for cancer prediction results

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -17,10 +17,10 @@ async function predictClassification(model, image) {
 
         if (confidenceScore > 50) {
             result = "Cancer";
-            suggestion = "Segera periksa ke dokter! Penting untuk melakukan pemeriksaan lebih lanjut untuk memastikan diagnosis";
+            suggestion = "Segera periksa ke dokter!";
         }else {
             result = "Non-cancer";
-            suggestion = "Anda sehat! Tetap jaga pola hidup sehat untuk menjaga kesehatan Anda";
+            suggestion = "Penyakit kanker tidak terdeteksi.";
         }
 
         return { result, suggestion };
